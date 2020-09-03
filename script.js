fbb0ad9558840a313c834fb5d217a13370140f03
arsvechkarev <arsvechkarev@gmail.com>
2020-09-03 19:10:15 +0300

    Updated javascript

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const TABLE_COUNRIES = 'table_countries'
+const TABLE_COUNTRIES = 'table_countries'
+const TABLE_CELL = 'table_cell'
 
 const COUNTRIES_KEY = 'Countries'
 const COUNTRY_NAME_KEY = 'Country'
+const COUNTRY_CODE_KEY = 'CountryCode'
 const CONFIRMED_KEY = 'TotalConfirmed'
 const RECOVERED_KEY = 'TotalRecovered'
 const DEATHS_KEY = 'TotalDeaths'
@@ -18,7 +20,7 @@ function createSortedList(json) {
 }
 
 function renderTable(countries) {
-  const table = document.getElementById(TABLE_COUNRIES)
+  const table = document.getElementById(TABLE_COUNTRIES)
 
   for (let i = 0; i < countries.length; i++) {
     const row = table.insertRow()
@@ -29,35 +31,27 @@ function renderTable(countries) {
     const deathsCell = createStyledCell(row)
     numberCell.appendChild(document.createTextNode(`${(i + 1)}.`))
     countryCell.appendChild(document.createTextNode(countries[i][COUNTRY_NAME_KEY]));
-    appendNumber(confirmedCell, countries[i][CONFIRMED_KEY])
-    appendNumber(recoveredCell, countries[i][RECOVERED_KEY])
-    appendNumber(deathsCell, countries[i][DEATHS_KEY])
+    appendNumber(confirmedCell, countries[i], CONFIRMED_KEY)
+    appendNumber(recoveredCell, countries[i], RECOVERED_KEY)
+    appendNumber(deathsCell, countries[i], DEATHS_KEY)
   }
 }
 
 function createStyledCell(row) {
   const cell = row.insertCell()
-  cell.setAttribute('class', 'table_cell')
+  cell.setAttribute('class', TABLE_CELL)
   return cell
 }
 
-function appendNumber(cell, number) {
+// Countries that for whatever reason don't provide recovered data.
+// Instead of recovered number symbol `—` should be displayed instead  
+const countriesWithoutRecovered = ['SE', 'RS', 'NL']
+
+function appendNumber(cell, country, key) {
   cell.style.textAlign = 'right'
-  cell.appendChild(document.createTextNode(format(number)));
-
-  function format(number) {
-    const result = []
-    const string = number.toString()
-    let i = string.length;
-    counter = 1
-    while (i--) {
-      result.push(string[i])
-      if ((counter) % 3 == 0) {
-        result.push(' ')
-      }
-      counter++
-    }
-    return result.reverse().join('')
+  let text = country[key].toLocaleString()
+  if (key == RECOVERED_KEY && countriesWithoutRecovered.includes(country[COUNTRY_CODE_KEY])) {
+    text = '—\xa0\xa0\xa0\xa0'
   }
+  cell.appendChild(document.createTextNode(text));
 }
-
